84774ce9f4d6fa2b05a0d4e80b6c2db9b5e8c8f2
gustavo-sm <gustavo1sad@gmail.com>
2024-02-27 04:36:02 -0300

    Implentando producer generico

---
+++ src/main/java/com/example/demo/services/kafka/producer/KafkaProducer.java
@@ -0,0 +1,28 @@
+package com.example.demo.services.kafka.producer;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("topicotest", msg);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Sent message=[" + msg +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        msg + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+}
